3256cc5c6d93cbeca584f00c6d2e33bb8740273d
Knarkzel <knarkzel@gmail.com>
2021-07-12 06:02:21 +0200

    consistent examples

--- examples/pad.rs
+++ examples/pad.rs
@@ -6,30 +6,44 @@ use ogc::prelude::*;
 
 #[start]
 fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    // Boilerplate
+    // Initialise the video system
     let video = Video::init();
+
+    // Initialise the console, required for print.
     Console::init(&video);
+
+    // Set up the video registers with the chosen mode.
     Video::configure(video.render_config.into());
+
+    // Tell the video hardware where our display memory is.
     Video::set_next_framebuffer(video.framebuffer);
+
+    // Make the display visible.
     Video::set_black(false);
+
+    // Flush the video register changes to the hardware.
     Video::flush();
+
+    // Wait for Video setup to complete.
     Video::wait_vsync();
 
-    // Pad
+    // Initialise the pad
     Pad::init();
 
     loop {
+        // Scan all pads
         Pad::scan_pads();
 
-        // Check for pressed buttons, fancy way
-        let output = match Pad::buttons_down(Controller::One) {
-            Button::A => "A was pressed!",
-            Button::B => "B was pressed!",
-            Button::X => "X was pressed!",
-            Button::Y => "Y was pressed!",
-            _ => "",
-        };
-        println!("{}", output);
+        // Check for pressed buttons
+        let pressed = Pad::buttons_down(Controller::One);
+
+        if pressed == Button::A {
+            println!("A was pressed");
+        }
+
+        if pressed == Button::B {
+            println!("B was pressed");
+        }
 
         // Check for released buttons
         let released = Pad::buttons_up(Controller::One);
@@ -80,7 +94,7 @@ fn main(_argc: isize, _argv: *const *const u8) -> isize {
             println!("Triggers: ({}, {})", trigger_l, trigger_r);
         }
 
-        // Boilerplate
+        // Wait for the next frame.
         Video::wait_vsync();
     }
 }
